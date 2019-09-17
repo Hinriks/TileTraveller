37ffc25bcc24b1fa965bc408c90a915d501085c2
Hinrik Snær <hinni97@hotmail.com>
2019-09-17 09:46:17 +0000

    Added algorithm as a comment

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,3 +1,6 @@
 """ 
-
+tvö variables, x og y, sem fer frá 1 upp í 3 (sem myndar 3x3 grid). 
+staðsetning leikmans er x,y og kemst ekki út fyrir 3x3 gridið (athuga með if statement).
+Leikmaður getur farið á milli reita nema þegar það er lokaður veggur í þá átt sem hann vill fara (athuga með if statement). 
+Leikmaður byrjar á reit 1,1 og vinnur leikinn á reit 3,1.
 """
