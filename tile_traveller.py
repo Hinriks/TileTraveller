73cdd3b65b586d3aa93d3f50b4dd405e7e88aaa2
Hinrik Snær <hinni97@hotmail.com>
2019-09-17 11:58:14 +0000

    Added code, everything working but "or" between

--- tile_traveller.py
+++ tile_traveller.py
@@ -8,13 +8,9 @@ Leikmaður byrjar á reit 1,1 og vinnur leikinn á reit 3,1.
 x_pos = 1
 y_pos = 1
 
-def player_position(x,y):
-    """
-    changes the position of the player
-    """
-    
-def position_checker(n, e, s, w):
-    """
+
+"""def position_checker(x, y):
+
     if y == 1:
         y += 1
     elif y == 2:
@@ -29,6 +25,119 @@ def position_checker(n, e, s, w):
             x += 1 
             x -= 1
     """
+
+
+def check_victory(x, y):
+    if x == 3 and y == 1:
+        return True
+    else:
+        return False
+
+def grid_border_checker(x, y):
+    if x > 3:
+        x = 3
+        print ("Not a valid direction!")
+    elif x < 1:
+        x = 1
+        print ("Not a valid direction!")
+    if y > 3:
+        y = 3
+        print ("Not a valid direction!")
+    elif y < 1:
+        y = 1
+        print ("Not a valid direction!")
+    
+    return x,y
+
+def grid_wall_checker(x, y):
+    """if y == 1:
+        
+    elif y == 2:
+        if x == 2:
+            x -= 1
+            y -= 1
+        elif x == 3:
+            y += 1
+            y -= 1
+    elif y == 3:
+        if x== 2:
+            x += 1 
+            x -= 1"""
+
+def invalid_direction():
+    print("Not a valid direction!")
+
+west = True
+east = True
+south = True
+north = True
+
+def allowed_movements(x, y, w, e, s, n):
+    print ("You can travel: ", end="")
+    if x > 1:
+        if x == 3 and y == 2:
+            w = False
+        elif x == 3 and y == 1:
+            w = False
+        elif x == 2 and y == 1:
+            w = False
+        else:
+            print("(W)est", end=" ")
+    else:
+        w = False
+    if x < 3:
+        if x == 1 and y == 1:
+            e = False
+        elif x == 2 and y == 1:
+            e = False
+        elif x == 2 and y == 2:
+            e = False
+        else:
+            print("(E)ast", end=" ")
+    else:
+        e = False
+    if y > 1:
+        if x == 2 and y == 3:
+            s = False
+        else:
+            print("(S)outh", end=" ")
+    else:
+        s = False
+    if y < 3:
+        if x == 2 and y == 2:
+            n = False
+        else:
+            print("(N)orth", end=" ")
+    else:
+        n = False
+    print (".")
+
+    return w,e,s,n
+
+while True:
+    west,east,south,north = allowed_movements(x_pos, y_pos, west, east, south, north)
+
+    user_input = input("Direction: ")
+    if user_input == "n" and north:
+        y_pos += 1
+    elif user_input == "e" and east:
+        x_pos += 1
+    elif user_input == "s" and south:
+        y_pos -= 1
+    elif user_input == "w" and west:
+        x_pos -= 1
+    else:
+        invalid_direction()
+
+    x_pos, y_pos = grid_border_checker(x_pos, y_pos)
+
+
+    if check_victory(x_pos, y_pos):
+        print ("Victory!")
+    
+    west,east,south,north = True,True,True,True
+
+
 
 """while available position is entered
     change position
