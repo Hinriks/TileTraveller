4c11745217a2dcd83f9b13a44c81b99a9de7ae5c
Jóhann Ingi Skúlason <joi@desktop-6dvvas1.hir.is>
2019-09-17 10:34:03 +0000

    addded skeleton

--- tile_traveller.py
+++ tile_traveller.py
@@ -4,3 +4,32 @@ staðsetning leikmans er x,y og kemst ekki út fyrir 3x3 gridið (athuga með if
 Leikmaður getur farið á milli reita nema þegar það er lokaður veggur í þá átt sem hann vill fara (athuga með if statement). 
 Leikmaður byrjar á reit 1,1 og vinnur leikinn á reit 3,1.
 """
+
+x_pos = 1
+y_pos = 1
+
+def player_position(x,y):
+    """
+    changes the position of the player
+    """
+    
+def position_checker(n, e, s, w):
+    """
+    if y == 1:
+        y += 1
+    elif y == 2:
+        if x == 2:
+            x -= 1
+            y -= 1
+        elif x == 3:
+            y += 1
+            y -= 1
+    elif y == 3:
+        if x== 2:
+            x += 1 
+            x -= 1
+    """
+
+"""while available position is entered
+    change position
+    if not do not change position and print unavailable"""
